417686b45f4ad37933b576f8c7daa25e007db076
Yuya Ohbayashi <yuya9445@gmail.com>
2021-10-27 08:32:58 +0900

    32. ハンズオン　完了処理を作りましょう　～リダイレクトとリクエストスコープ～

--- WebBeginnerStarter/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -32,7 +33,8 @@ public class InquiryController {
 	}
 
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm, Model model) {
+	public String form(InquiryForm inquiryForm, Model model,
+			@ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
 	}
