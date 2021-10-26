7a72a279ac2581b19cceb35a34533ec1dd74f760
Yuya Ohbayashi <yuya9445@gmail.com>
2021-10-26 21:54:41 +0900

    29. ハンズオン　確認ページを作りましょう　～バリデーション機能～

--- WebBeginnerStarter/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -2,6 +2,8 @@ package com.example.demo.app.inquiry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +31,7 @@ public class InquiryController {
 	}
 
 	@GetMapping("/form")
-	public String form(Model model) {
+	public String form(InquiryForm inquiryForm, Model model) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
 	}
@@ -42,10 +44,15 @@ public class InquiryController {
 
 
 	@PostMapping("/confirm")
-	public String confirm(/*Add parameters. */) {
+	public String confirm(@Validated InquiryForm inquiryForm,
+			BindingResult result,
+			Model model) {
 
-		//hands-on
-
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form";
+		}
+		model.addAttribute("title", "Confirm Page");
 		return "inquiry/confirm";
 	}
 
