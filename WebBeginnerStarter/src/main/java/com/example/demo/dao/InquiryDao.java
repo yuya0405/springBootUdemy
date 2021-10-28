77e817df706cb75a47486fdebd43585c27856e47
Yuya Ohbayashi <yuya9445@gmail.com>
2021-10-29 07:04:02 +0900

    40. ハンズオン　DAOパターンを理解しましょう　～DAOクラスの作成～

---
+++ WebBeginnerStarter/src/main/java/com/example/demo/dao/InquiryDao.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.entity.Inquiry;
+
+public interface InquiryDao {
+	
+	void insertInquiry(Inquiry inquiry);
+	
+	List<Inquiry> getAll();
+
+}
